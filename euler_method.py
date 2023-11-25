4cb507677d7484ae71fdfe6ba61741dcd5501a06
niko <91669604+qtniko@users.noreply.github.com>
2023-11-25 20:16:08 +0100

    Create euler_method.py

---
+++ euler_method.py
@@ -0,0 +1,22 @@
+def euler(x0, y0, x, h, df):
+    xn = x0
+    yn = y0
+    while xn < x:
+        xn += h
+        yn += h * df(yn)
+    return yn
+
+
+
+r = 2
+K = 1000
+N0 = 100
+
+df = lambda nt: \
+    r * nt * (1 - nt/K)
+
+t = 1
+h = 1
+
+y = euler(0, N0, t, h, df)
+print(y)
