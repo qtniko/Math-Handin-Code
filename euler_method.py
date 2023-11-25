19d834a305b6a73dbe7bb4e7d3f65c0f41524c30
niko <91669604+qtniko@users.noreply.github.com>
2023-11-25 20:22:07 +0100

    Update euler_method.py

--- euler_method.py
+++ euler_method.py
@@ -7,16 +7,15 @@ def euler(x0, y0, x, h, df):
     return yn
 
 
-
 r = 2
-K = 1000
+K = 10000
 N0 = 100
 
 df = lambda nt: \
     r * nt * (1 - nt/K)
 
 t = 1
-h = 1
+h = 0.01
 
 y = euler(0, N0, t, h, df)
 print(y)
